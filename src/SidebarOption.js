cbdbfa992c2b7775b840d1c9c07c04ca0e92673d
unknown <>
2020-11-28 16:06:25 -0600

    fixing bugs to make home option active

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -3,7 +3,7 @@ import "./SidebarOption.css";
 
 function SidebarOption({active, text, Icon}) {
   return (
-    <div className= {`sidebarOption ${active && 'sidebarOption--active'}`}>
+    <div className= {`sidebarOption ${active && "sidebarOption--active"}`}>
       <Icon />
       <h2>{text}</h2>
 
