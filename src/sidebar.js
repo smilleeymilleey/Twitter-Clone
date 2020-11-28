94d5bd0c01383eb9263eaf34e79d00868ce83f77
unknown <>
2020-11-28 13:32:01 -0600

    added a twitter logo

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,12 +1,13 @@
 import React from "react";
-import '.Sidebar.css';
-import TwitterIcon from '@material-ui/icons/Twitter'
+import './Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 function Sidebar() {
   return (
     <div className="Sidebar">
     {/*Twitter Icon*/}
     <TwitterIcon />
+
     {/* Sidebar Option*/}
     {/* Sidebar Option*/}
     {/* Sidebar Option*/}
