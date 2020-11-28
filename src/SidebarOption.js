aaa11fcec45d228f5caca56924ee3a77f2eec09d
unknown <>
2020-11-28 15:08:37 -0600

    sidebar is built, and changed cursor to a pointer

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./SidebarOption.css";
 
-function SidebarOption({ text, Icon }) {
+function SidebarOption({text, Icon}) {
   return (
     <div className="sidebarOption">
       <Icon />
--- src/sidebar.js
+++ src/sidebar.js
@@ -5,6 +5,11 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from '@material-ui/icons/Search';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 
 
@@ -17,12 +22,14 @@ function Sidebar() {
 
 
     <SidebarOption Icon={HomeIcon} text="Home" />
-
     <SidebarOption Icon={SearchIcon} text="Explore" />
-
     <SidebarOption  Icon={NotificationsNoneIcon} text="Notifications" />
+    <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+    <SidebarOption Icon={BookmarkBorderIcon} text="Bookmark" />
+    <SidebarOption Icon={ListAltIcon} text="Lists" />
+    <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+    <SidebarOption Icon={MoreHorizIcon} text="More" />
 
-    
 
 
 
