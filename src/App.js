de801854c2bb257dca0367d003be61aa9d707ef1
unknown <>
2020-11-27 18:17:45 -0600

    creating a sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
+import React from "react";
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Twitter Clone</h1>
+    //BEM
+    <div className="app">
+      <h1>Twitter Clone</h1>
+
+      {/* SideBar */}
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widgets */}
+
+
+
+
+
     </div>
   );
 }
---
+++ src/sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import '.Sidebar.css';
+import TwitterIcon from '@material-ui/icons/Twitter'
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+    {/*Twitter Icon*/}
+    <TwitterIcon />
+    {/* Sidebar Option*/}
+    {/* Sidebar Option*/}
+    {/* Sidebar Option*/}
+    {/* Sidebar Option*/}
+    {/* Sidebar Option*/}
+    {/* Sidebar Option*/}
+
+    {}
+
+    </div>
+  );
+
+}
+
+export default Sidebar;
