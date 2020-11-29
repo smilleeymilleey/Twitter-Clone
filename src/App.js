a95b892bec08da00bf8ace125c72ae2cc888d4c6
unknown <>
2020-11-29 16:08:54 -0600

    hid the scroll bar and added widget css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
+import Widgets from './Widgets';
 import './App.css';
 
 function App() {
   return (
     //BEM
     <div className="app">
-    
+
       <Sidebar />
 
       {/* Feed */}
       <Feed />
       {/* Widgets */}
-
+      <Widgets />
 
 
 
