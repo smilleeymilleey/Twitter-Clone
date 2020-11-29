0071fd1fae3ba484d5d258a487b836080b904734
unknown <>
2020-11-29 17:13:42 -0600

    added a tweet box, still needs styling

--- src/Feed.js
+++ src/Feed.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import TweetBox from './TweetBox';
 import './Feed.css';
+
 
 function Feed() {
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
+        </div>
       {/* TweetBox */}
-    </div>
+      <TweetBox />
+  
     </div>
   );
 }
---
+++ src/TweetBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+
+function TweetBox() {
+  return (
+    <div className="tweetBox">
+      <form>
+       <div className="tweetBox_input">
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          <input placeholder="Whats happening?" type="text"/>
+          {/*<input placeholder="Enter Image URL" type="text"/> */}
+       </div>
+      <Button>Tweet</Button>
+     </form>
+    </div>
+  );
+}
+
+export default TweetBox;
