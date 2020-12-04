bff0dc6e6da9b59623ced49ee6661d2d91d56c31
unknown <>
2020-12-04 11:50:38 -0600

    added buttons to a post

--- src/Post.js
+++ src/Post.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
-import VerifiedUserIcon from "@material-ui/icons/VerifiedUserIcon";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
+
 
 
 function Post({displayName, username, verified, text, image, avatar}) {
@@ -17,13 +22,20 @@ function Post({displayName, username, verified, text, image, avatar}) {
               <h3>
                 Caitlyn Miley <span>
                 <VerifiedUserIcon className="post_badge" />
-                </span>
+                </span> @smilleymilleey
               </h3>
-            constructor() {
-
-            }
-
+              <div className="post_headerdescription">
+                <p> I challenge you to build a twitter clone</p>
+              </div>
             </div>
+            <img src="https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif" alt="" />
+            <div className= "post_footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+              <RepeatIcon fontSize="small" />
+              <FavoriteBorderIcon fontSize="small" />
+              <PublishIcon fontsize="small" />
+            </div>
+
           </div>
         </div>
       </div>
