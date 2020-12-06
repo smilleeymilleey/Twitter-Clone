cea902a6b865e823fb09a0a90e28b0a010bfbeda
unknown <>
2020-12-06 13:54:46 -0600

    created backed functionality using firebase

--- src/Feed.js
+++ src/Feed.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import TweetBox from './TweetBox';
 import './Feed.css';
 import Post from './Post';
+import db from './firebase';
 
 
 function Feed() {
+  const [posts, setPosts] = useState ([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => (
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    ))
+
+  }, []);
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
         </div>
-      {/* TweetBox */}
+
       <TweetBox />
-      <Post displayName="Caitlyn"
-      username="smilleeymilleey"
-      verified={true}
-      text="its working"
-      avatar="/static/images/avatar/3.jpg"
-      image='https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif'/>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+        {posts.map(post=> (
+        <Post
+         displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+        ))}
+
     </div>
   );
 }
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./TweetBox.css";
 import { Avatar } from "@material-ui/core";
+import db from'./firebase';
 
 function TweetBox() {
+const [tweetMessage, setTweetMessage] = useState("");
+const [tweetImage, setTweetImage] = useState("");
+
+const sendTweet = e => {
+  e.preventDefault();
+
+  db.collection('posts').add({
+    displayName: "Caitlyn Miley",
+    username: "smileymiley",
+    verified: true,
+    text: tweetMessage,
+    image: tweetImage,
+    avatar: "/static/images/avatar/3.jpg"
+  });
+
+  setTweetMessage("");
+  setTweetImage("");
+};
+
+
   return (
     <div className="tweetBox">
       <form>
        <div className="tweetBox_input">
           <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          <input placeholder="Whats happening?" type="text"/>
+          <input
+           onChange={e => setTweetMessage(e.target.value)}
+           value={tweetMessage}
+           placeholder="Whats happening?"
+           type="text"/>
        </div>
-         <input className="tweetbox_imageInput" placeholder="Optional: Enter Image URL" type="text"/>
+         <input
+          onchange={e => setTweetImage (e.target.value)}
+          value={tweetImage}
+          className="tweetbox_imageInput"
+          placeholder="Optional: Enter Image URL"
+          type="text"/>
 
-      <button className="tweetBox_tweetButton">Tweet</button>
+      <button onClick={sendTweet} className="tweetBox_tweetButton">Tweet</button>
      </form>
     </div>
   );
